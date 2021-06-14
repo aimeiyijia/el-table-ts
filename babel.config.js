556c00a2d6199d14e83343990af7b656dafc7b1c
aimeiyijia <1456723200@qq.com>
2021-06-14 15:11:21 +0800

    调整babel

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,36 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    '@vue/cli-plugin-babel/preset',
+    [
+      '@babel/env',
+      {
+        useBuiltIns: 'usage',
+        corejs: {
+          version: 3,
+          proposals: true, // 使用尚在“提议”阶段特性的 polyfill
+        },
+      },
+    ],
+    '@babel/preset-typescript',
   ],
   env: {
     development: {
       // babel-plugin-dynamic-import-node plugin only does one thing by converting all import() to require().
       // This plugin can significantly increase the speed of hot updates, when you have a large number of pages.
       // https://panjiachen.github.io/vue-element-admin-site/guide/advanced/lazy-loading.html
-      plugins: ['dynamic-import-node']
-    }
-  }
+      plugins: [
+        'dynamic-import-node',
+        // 以下插件具有先后顺序
+        // 首先编译装饰器语法
+        [
+          '@babel/plugin-proposal-decorators',
+          {
+            legacy: true,
+          },
+        ],
+        // 再编译 class 语法
+        '@babel/plugin-proposal-class-properties',
+      ],
+    },
+  },
 }
