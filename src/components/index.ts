96b2cf3456f197605090c1f7af977e0f344f1a5d
aimeiyijia <1456723200@qq.com>
2021-07-23 17:41:13 +0800

    调整导出方式

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,21 +1,27 @@
 import ElTableTs from './el-table-ts'
 import ElTableHttp from './el-table-http'
-import _Vue, { PluginFunction } from 'vue'
+import _Vue, { PluginFunction, VueConstructor } from 'vue'
 
 interface InstallFunction extends PluginFunction<any> {
   installed?: boolean;
 }
 
+const Components: { [key: string]: VueConstructor } = {
+  ElTableTs: ElTableTs,
+  ElTableHttp: ElTableHttp,
+}
+
 const install: InstallFunction = (Vue: typeof _Vue) => {
   if (install.installed) return
 
-  Vue.component(ElTableTs.name, ElTableTs)
-  Vue.component(ElTableHttp.name, ElTableHttp)
+  Object.keys(Components).forEach((name: any) => {
+    Vue.component(name, Components[name]) // X
+  })
+
   install.installed = true
 }
 
 export default install
-
 
 // export default {
 //   install(Vue: typeof _Vue) {
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,2 @@
-import { PluginFunction } from 'vue'
-declare module 'el-table-ts' {
-  export const install: PluginFunction<{}>;
-}
+
+declare module 'el-table-ts'
