1cb54df50d4ef2c373c01df2b070213c2c78542c
aimeiyijia <1456723200@qq.com>
2022-02-17 16:28:53 +0800

    feat：调整下类型声明

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,27 +1,51 @@
 
-import Vue from 'vue'
+import Vue, { CreateElement, VNode } from 'vue'
 import { AxiosRequestConfig } from 'axios'
-export declare class Path {
+import { ElTableColumn } from 'element-ui/types/table-column'
+
+
+// 默认分页
+export declare interface IDefPagination {
+  currentPage: number
+  pageSizes: number[]
+  pageSize: number
+  layout: string
+  background: boolean
+}
+
+// 指令
+export declare interface IDirectives {
+  heightAdaptive?: {
+    bottomOffset: number
+  }
+}
+
+export declare interface IPagEventCallbackParams {
+  pageSize: number
+  currentPage: number
+}
+
+export declare interface IPath {
   // data的解析路径,不指定就按照默认路径去解析
   dataPath: string
   // 不指定就默认dataName为data
   dataName: string
 }
 
-export declare class Pag {
+export declare interface IPag {
   // 条数指示器名称
   pageSizeName: string
   // 页码参数名称
   pageNoName: string
 }
-export declare class NetWork {
+export declare interface INetWork {
   method?: string
   url?: string
   data?: any
   createConfig?: AxiosRequestConfig
   httpConfig?: AxiosRequestConfig
-  path?: Path
-  pag?: Pag
+  path?: IPath
+  pag?: IPag
 }
 
 // for future
@@ -30,3 +54,27 @@ export declare class NetWork {
 
 // now
 export function install(vue: typeof Vue): void
+
+
+export declare class ElTableTsColumn extends ElTableColumn {
+  customRender?: (cellValue: any,
+    row: object,
+    column: ElTableColumn,
+    $index: number,
+    h: CreateElement,
+    store: any,
+    _self: VNode) => any
+
+  customTitle?: (cellValue: any,
+    row: object,
+    column: ElTableColumn,
+    $index: number,
+    h: CreateElement,
+    store: any,
+    _self: VNode) => any
+
+    scopedSlots?: {
+      customRender?: string
+      customTitle?: string
+    }
+}
